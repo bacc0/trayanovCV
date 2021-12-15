eebc793c8de9d4b7c1be61ab090d1d9c1357eee6
Veselin Hr. Trayanov <1.1@gmx.com>
2021-12-15 17:57:37 +0000

    move left

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fd4ae93e52f2863a"; }
+/******/ 		__webpack_require__.h = function() { return "6655b95dfc37ec3e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
