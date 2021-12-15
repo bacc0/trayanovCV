eb8e049b3989ea5e55203a605da17d01a509afdc
Veselin Hr. Trayanov <1.1@gmx.com>
2021-12-15 18:01:49 +0000

    W-H removed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6655b95dfc37ec3e"; }
+/******/ 		__webpack_require__.h = function() { return "9f0e45cf7fe95172"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -34,8 +34,8 @@ export default function Home() {
      return (
           <section className='pages-wrapper'>
              < HomePage W_screenSize={W_screenSize}/>
-             {W_screenSize}--
-             {H_screenSize}
+             {/* {W_screenSize}--
+             {H_screenSize} */}
              {/* < Tech/>
              < About/> */}
 
