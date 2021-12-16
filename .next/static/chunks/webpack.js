2b9c2483aa6310029919272182d6f4229eebb4bd
Veselin Hr. Trayanov <1.1@gmx.com>
2021-12-16 00:34:33 +0000

    New animation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d3d5346196200a88"; }
+/******/ 		__webpack_require__.h = function() { return "cee89945895bf50e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
