57c00792780df84ac06b67c1a9121ec38d5bf9d6
Veselin Hr. Trayanov <1.1@gmx.com>
2021-12-16 23:19:37 +0000

    navbar transparent position

---
+++ _components/_sections/AboutMe.js
@@ -0,0 +1,86 @@
+import { motion } from 'framer-motion'
+
+
+
+export default function AboutMe({
+     min_width_600px, backgroundColor, hrColorMain, thirdLettersColor, hrColor
+}) {
+
+     return (
+          <div>
+               <div className='page-body'>
+                    <div className='page-body-left'>
+
+                         <div className='container-content-left' >
+                              <div
+                                   className='content-left'
+                                   // style={{ marginTop: min_width_600px ? 0 : 16 }}
+                                   style={{ margin: `${min_width_600px ? 0 : -20}px 0 ${min_width_600px ? 36 : -7}px` }}
+                              >
+                                   <motion.div
+                                        initial={{ scale: 1, x: -100, opacity: 0 }}
+                                        animate={{ scale: 1, x: 0, opacity: 1 }}
+                                        transition={{ type: 'spring', ease: 'anticipate', duration: 0.9, delay: 0 }}
+
+                                   >
+                                        <h3 style={{ color: backgroundColor, fontSize: 35 }}>
+                                             About me
+                                        </h3>
+                                        <hr style={{ backgroundColor: hrColorMain }} />
+                                   </motion.div>
+                              </div>
+                         </div>
+
+
+                         <div
+                              className='container-content-left'>
+                              <div className='content-left'
+                                   style={{ marginTop: min_width_600px ? 0 : 16 }}>
+                                   <h3 style={{ color: thirdLettersColor }}>
+                                        Skills
+                                   </h3>
+                                   <hr style={{ backgroundColor: hrColor }} />
+
+                                   <p>I’m very positive.</p>
+                                   <p>I’m very focus in my work.</p>
+                                   <p>
+                                        I’m very good on planning and organising my work, so my tasks will always be done on time in
+                                        necessary standard.
+                                   </p>
+                                   <p>
+                                        I’m happy working on my own, but I have also like to work as part of the team.
+                                   </p>
+
+
+
+                              </div>
+                         </div>
+                    </div>
+
+
+
+                    <div
+                         className='page-body-right'
+                         style={{ marginTop: min_width_600px ? -19 : -50 }}
+                    >
+                         <div className='container-content-right'>
+                              <div className='content-right'>
+
+                                   <p>Creativity, Flexibility, Responsibility.</p>
+                                   <p>Experience working with dynamic content.</p>
+                                   <p>Developing with ReactJS single page JavaScript Applications.</p>
+                                   <p>Problem Solving Skills.</p>
+
+                                   <p>Good understanding working with</p>
+                                   <p>APIs (RESTful services).</p>
+                                   <p>Web developer able to build a Web presence from the ground up, from concept, navigation, working with data and programming to UX.</p>
+                                   <p>Skilled at writing well-designed, efficient code using current best practices in Web development.</p>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+
+
+     )
+}
