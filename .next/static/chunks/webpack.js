6d132a9943346e1f2bd7cbf8407b4fbef25c5e16
Veselin Hr. Trayanov <1.1@gmx.com>
2021-12-19 02:59:01 +0000

    main logo change animation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc850f093e8759aa"; }
+/******/ 		__webpack_require__.h = function() { return "5826367ea38cc6a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
