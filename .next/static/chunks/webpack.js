c258517cd8e72a30f1c2179443ac95b88cd298e5
Veselin Hr. Trayanov <1.1@gmx.com>
2021-12-19 01:44:44 +0000

    Front end sign animation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "011ea4d0aa45b612"; }
+/******/ 		__webpack_require__.h = function() { return "58ca07db9149a25c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
