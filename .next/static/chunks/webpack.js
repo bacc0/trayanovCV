6c0e5fd0f3294f69be3eb15be9565602f39c5bb3
Veselin Hr. Trayanov <1.1@gmx.com>
2021-12-27 00:55:12 +0000

    //test branch REMOVE

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aa58ebbb38b0b1a9"; }
+/******/ 		__webpack_require__.h = function() { return "4b8a2ad929b414e6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
