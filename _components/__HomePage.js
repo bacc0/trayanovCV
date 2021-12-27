922a9f47cf6d8419c81e611dbe3f6681117b56ad
Veselin Hr. Trayanov <1.1@gmx.com>
2021-12-27 14:07:22 +0000

    var url background

--- _components/__HomePage.js
+++ _components/__HomePage.js
@@ -4,8 +4,8 @@ import MenuAppBar from './MenuAppBar'
 import PageNav from './PageNav'
 import { useState, useEffect, Fragment } from 'react'
 
-export default function HomePage({ 
-     currentBrowser, theme, setTheme, Y_position ,scrollDirection }) {
+export default function HomePage({
+     currentBrowser, theme, setTheme, Y_position, scrollDirection }) {
 
      const color_1 = '#FFFFFF'
      const color_2 = '#FAFAFA' // F2F2F2
@@ -29,15 +29,33 @@ export default function HomePage({
           bodyColor: color_5,
           footerColor: color_4,
           footerTextColor: color_1,
-          backgroundNav: `url('FRONT_END_2.svg'), url( 'backFrontEnd.svg' ), url( 'FRONT_END_2_COLOR.svg' )`,
+          backgroundNav_container: ``,
+          backgroundNav: ``
      }
      const [state, setState] = useState(INITIAL_STATE)
-     
+
      const [onAnimationEnd, setOnAnimationEnd] = useState(false)
 
      const min_width_600px = useMediaQuery('(min-width:600px)')
      const logo_W_H = 230
      const strokeWidth = min_width_600px ? 1.2 : 1.5
+
+     const backgroundNav_1_light = `url('FRONT_END_2.svg') `
+
+     const backgroundNav_1_dark = `url('FRONT_END_2_DARK.svg')`
+
+     // const [backgroundNav_2, setBackgroundNav_2] = useState(``)
+     // const [backgroundNav_3, setBackgroundNav_3] = useState(``)
+
+     // useEffect(() => {
+     //      setInterval(() => {
+     //           setBackgroundNav_2(`url('backFrontEnd.svg')`)
+     //           setBackgroundNav_3(`url('FRONT_END_2_COLOR.svg')`)
+     //      }, 1000);
+     // }, []);
+
+     const backgroundNav_2 = `url('backFrontEnd.svg')`
+     const backgroundNav_3 = `url('FRONT_END_2_COLOR.svg')`
 
      const animationTransition = '0ms'
 
@@ -45,37 +63,60 @@ export default function HomePage({
 
           setState({
                ...state,
-                    primeLettersColor: theme === 'light' ? color_1 : color_5,
-                    secLettersColor: theme === 'light' ? color_3 : color_2,
-                    thirdLettersColor: theme === 'light' ? color_6 : color_7,
-                    thirdLogoColor: theme === 'light' ? color_6 : color_7,
-                    strongText: theme === 'light' ? color_3 : color_2,
-                    hrColor: theme === 'light' ? color_2 : color_3,
-                    hrColorMain: theme === 'light' ? color_5 : color_1,
-                    backgroundColor: theme === 'light' ? color_5 : color_1,
-                    backgroundColor_2: theme === 'light' ? color_2 : color_4,
-                    AppBackgroundColor: theme === 'light' ? color_1 : color_4,
-                    bodyColor: theme === 'light' ? color_5 : color_1,
-                    footerColor: theme === 'light' ? color_4 : color_4,
-                    footerTextColor: theme === 'light' ? color_1 : color_1,
-                    backgroundNav: theme === 'light'
-                         ? `url('FRONT_END_2.svg'), url( 'backFrontEnd.svg' ), url( 'FRONT_END_2_COLOR.svg' )`
-                         : `url('FRONT_END_2_DARK.svg'), url( 'backFrontEnd.svg' ), url( 'FRONT_END_2_COLOR.svg'`,
-               }
-          )
-      
+               primeLettersColor: theme === 'light' ? color_1 : color_5,
+               secLettersColor: theme === 'light' ? color_3 : color_2,
+               thirdLettersColor: theme === 'light' ? color_6 : color_7,
+               thirdLogoColor: theme === 'light' ? color_6 : color_7,
+               strongText: theme === 'light' ? color_3 : color_2,
+               hrColor: theme === 'light' ? color_2 : color_3,
+               hrColorMain: theme === 'light' ? color_5 : color_1,
+               backgroundColor: theme === 'light' ? color_5 : color_1,
+               backgroundColor_2: theme === 'light' ? color_2 : color_4,
+               AppBackgroundColor: theme === 'light' ? color_1 : color_4,
+               bodyColor: theme === 'light' ? color_5 : color_1,
+               footerColor: theme === 'light' ? color_4 : color_4,
+               footerTextColor: theme === 'light' ? color_1 : color_1,
+               backgroundNav_container: theme === 'light'
+                    ? `${backgroundNav_2}, ${backgroundNav_3}`
+                    : `${backgroundNav_2}, ${backgroundNav_3}`,
+               backgroundNav: theme === 'light'
+                    ? `${backgroundNav_1_light}, ${backgroundNav_1_dark}`
+                    : `${backgroundNav_1_dark} , ${backgroundNav_1_light}`,
+          })
+
      }, [theme]);
 
-     const { AppBackgroundColor, hrColor, bodyColor, backgroundNav } = state
+     const { AppBackgroundColor, hrColor, bodyColor, backgroundNav_container, backgroundNav } = state
 
-     const styleBackgroundTitle = {
+     const bodyPage = {
+          backgroundColor: AppBackgroundColor,
+          // background: `radial-gradient(circle closest-side, #7026A5, #040F1B)`,
+          color: bodyColor,
+          transition: `background-color ${animationTransition} ease, 
+                                  color ${animationTransition} ease`,
+     }
+
+
+
+     const styleBackgroundTitle_container = {
+          // borderBottom: `1px solid ${hrColor}`,
           backgroundColor: AppBackgroundColor,
           width: '100%',
-          'background-image': min_width_600px ? backgroundNav : null,
-          backgroundPosition: `center -8px, center ${Number(20 + (Y_position * -1800)).toFixed(1)}px`,
+          'background-image': min_width_600px ? backgroundNav_container : null,
+          backgroundPosition: `center ${Number(20 + (Y_position * -1800)).toFixed(1)}px,
+                               center -8px`,
           backgroundSize: 536,
           backgroundRepeat: 'no-repeat',
-          borderBottom: `1px solid ${hrColor}`
+     }
+
+     const styleBackgroundTitle = {
+
+          width: '100%',
+          'background-image': min_width_600px ? backgroundNav : null,
+          backgroundPosition: `center -8px`,
+          backgroundSize: 536,
+          backgroundRepeat: 'no-repeat',
+          borderBottom: `1px solid ${hrColor}`,
      }
 
      return (
@@ -88,19 +129,21 @@ export default function HomePage({
                     scrollDirection={scrollDirection}
                     min_width_600px={min_width_600px}
                />
+
                <div
                     className='page'
-                    style={{
-                         backgroundColor: AppBackgroundColor,
-                         // background: `radial-gradient(circle closest-side, #7026A5, #040F1B)`,
-                         color: bodyColor,
-                         transition: `background-color ${animationTransition} ease, 
-                                                 color ${animationTransition} ease`,
-                    }}
+                    style={bodyPage}
                >
+                    {/* -------------------------------------------------------- */}
 
-                    <div className='page-nav'
-                         style={{ borderBottom: `1px solid ${hrColor}` }}>
+
+
+
+
+                    <div
+                         className='page-nav'
+                         style={styleBackgroundTitle_container}
+                    >
                          <div
                               className='title-container vertical-center-container'
                               style={styleBackgroundTitle}
@@ -115,6 +158,10 @@ export default function HomePage({
                          </div>
                     </div>
 
+
+
+
+                    {/* -------------------------------------------------------- */}
                     <CombineSections
                          state={state}
                          theme={theme}
