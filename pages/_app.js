2ad49f94293c915867c4c231c5814f61edb2bfc0
Veselin Hr. Trayanov <1.1@gmx.com>
2022-01-20 22:55:19 +0000

    Warning: Custom fonts removed at the document level

--- pages/_app.js
+++ pages/_app.js
@@ -8,15 +8,13 @@ function MyApp({ Component, pageProps }) {
 
                     <title>Veselin Trayanov</title>
 
-
-
                     <meta name="theme-color" content="#3F3D56" />
                     {/* <meta name="theme-color" media="(prefers-color-scheme: light)" content="#a2f5f3"></meta>
                         <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#111111"></meta> */}
 
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"
+                    {/* <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"
 
-                    />
+                    /> */}
                </Head>
                <Component {...pageProps} />
 
